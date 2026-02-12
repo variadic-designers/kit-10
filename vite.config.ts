f758be37d24b3c64c0edbe9d7cdf28ae1174b30c
YorQat <qarkdev+gh@gmail.com>
2026-02-12 15:01:07 +0800

    trim packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
-import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { defineConfig } from 'vitest/config';
-import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
 import k10 from './plugin/vite-kit10-plugin.js';
 
@@ -10,7 +8,6 @@ export default defineConfig({
 		k10(),
 		sveltekit(),
 		devtoolsJson(),
-		paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide' })
 	],
 
 	test: {
@@ -22,13 +19,6 @@ export default defineConfig({
 
 				test: {
 					name: 'client',
-
-					browser: {
-						enabled: true,
-						provider: playwright(),
-						instances: [{ browser: 'chromium', headless: true }]
-					},
-
 					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
 					exclude: ['src/lib/server/**']
 				}
