685f68c3ba4fef2fcf4cc2b46226d711a05b73e3
YorQat <qarkdev+gh@gmail.com>
2026-05-11 11:14:26 +0800

    pglite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,30 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { defineConfig } from 'vitest/config';
+import { searchForWorkspaceRoot } from 'vite';
+
 import { sveltekit } from '@sveltejs/kit/vite';
 import k10 from './plugin/vite-kit10-plugin.js';
 
 export default defineConfig({
 	plugins: [k10(), sveltekit(), devtoolsJson()],
+
+	optimizeDeps: {
+		exclude: ['@electric-sql/pglite']
+	},
+
+	worker: {
+		format: 'es'
+	},
+
+	server: {
+		headers: {
+			'Cross-Origin-Embedder-Policy': 'require-corp',
+			'Cross-Origin-Opener-Policy': 'same-origin'
+		},
+		fs: {
+			allow: [searchForWorkspaceRoot(process.cwd())]
+		}
+	},
 
 	test: {
 		expect: { requireAssertions: true },
