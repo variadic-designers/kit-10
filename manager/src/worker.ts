c57430603cae9dddbb5d9e9e1448891eca318858
YorQat <qarkdev+gh@gmail.com>
2026-06-10 10:22:06 +0800

    revert: restore OPFS-AHP, idb is too slow

--- manager/src/worker.ts
+++ manager/src/worker.ts
@@ -1,4 +1,5 @@
 import { PGlite } from '@electric-sql/pglite';
+import { OpfsAhpFS } from '@electric-sql/pglite/opfs-ahp';
 
 import { pg_uuidv7 } from '@electric-sql/pglite/pg_uuidv7';
 import { worker } from '@electric-sql/pglite/worker';
@@ -7,7 +8,11 @@ import { live } from '@electric-sql/pglite/live';
 worker({
 	async init() {
 		return new PGlite({
-			dataDir: 'idb://kit10-editor',
+			dataDir: 'kit10-editor',
+			fs: new OpfsAhpFS('kit10-editor', {
+				initialPoolSize: 50,
+				maintainedPoolSize: 25
+			}),
 			extensions: { pg_uuidv7, live }
 		});
 	}
