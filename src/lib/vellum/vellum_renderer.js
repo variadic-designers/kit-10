2e984998d20b7855850f375386d790801f47db61
YorQat <qarkdev+gh@gmail.com>
2026-07-14 06:13:34 +0800

    feat: hover-time box-model hatch overlay in vellum (padding + gap)
    Rebuilt vellum wasm + glue. Hovering a node underlays a diagonal-hatch fill on
    its padding ring and, for a box with gap, the gap tracks between children.
    Vellum-internal; hover already flows via set_data.

--- src/lib/vellum/vellum_renderer.js
+++ src/lib/vellum/vellum_renderer.js
@@ -532,7 +532,7 @@ function __wbg_get_imports() {
                     const a = state0.a;
                     state0.a = 0;
                     try {
-                        return __wasm_bindgen_func_elem_8670(a, state0.b, arg0, arg1);
+                        return __wasm_bindgen_func_elem_8716(a, state0.b, arg0, arg1);
                     } finally {
                         state0.a = a;
                     }
@@ -1152,12 +1152,12 @@ function __wbg_get_imports() {
         }, arguments); },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
             // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 315, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
-            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_7263);
+            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_7309);
             return addHeapObject(ret);
         },
         __wbindgen_cast_0000000000000002: function(arg0, arg1) {
             // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 364, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
-            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_8668);
+            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_8714);
             return addHeapObject(ret);
         },
         __wbindgen_cast_0000000000000003: function(arg0) {
@@ -1189,14 +1189,14 @@ function __wbg_get_imports() {
     };
 }
 
-function __wasm_bindgen_func_elem_7263(arg0, arg1, arg2) {
-    wasm.__wasm_bindgen_func_elem_7263(arg0, arg1, addHeapObject(arg2));
+function __wasm_bindgen_func_elem_7309(arg0, arg1, arg2) {
+    wasm.__wasm_bindgen_func_elem_7309(arg0, arg1, addHeapObject(arg2));
 }
 
-function __wasm_bindgen_func_elem_8668(arg0, arg1, arg2) {
+function __wasm_bindgen_func_elem_8714(arg0, arg1, arg2) {
     try {
         const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-        wasm.__wasm_bindgen_func_elem_8668(retptr, arg0, arg1, addHeapObject(arg2));
+        wasm.__wasm_bindgen_func_elem_8714(retptr, arg0, arg1, addHeapObject(arg2));
         var r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
         var r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
         if (r1) {
@@ -1207,8 +1207,8 @@ function __wasm_bindgen_func_elem_8668(arg0, arg1, arg2) {
     }
 }
 
-function __wasm_bindgen_func_elem_8670(arg0, arg1, arg2, arg3) {
-    wasm.__wasm_bindgen_func_elem_8670(arg0, arg1, addHeapObject(arg2), addHeapObject(arg3));
+function __wasm_bindgen_func_elem_8716(arg0, arg1, arg2, arg3) {
+    wasm.__wasm_bindgen_func_elem_8716(arg0, arg1, addHeapObject(arg2), addHeapObject(arg3));
 }
 
 
